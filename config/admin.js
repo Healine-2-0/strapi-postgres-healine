60d852557e281c5286b96b1569c42324199b824d
Jobin Shibu <jobinshibu.pk@gmail.com>
2025-08-27 15:40:28 +0530

    api token salt

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,20 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
+    secret: env('ADMIN_JWT_SECRET', 'your-admin-jwt-secret'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'your-generated-api-token-salt'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'your-generated-transfer-token-salt'),
+    },
+  },
+  secrets: {
+    encryptionKey: env('ENCRYPTION_KEY', 'your-generated-encryption-key'),
+  },
+  flags: {
+    nps: env.bool('FLAG_NPS', true),
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
